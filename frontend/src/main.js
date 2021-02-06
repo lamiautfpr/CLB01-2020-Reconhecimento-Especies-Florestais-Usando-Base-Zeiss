8d11d3ef85d8b27df26be701df1395268a11a1e8
Gustavo Silva Quieregato <quieregatog@alunos.utfpr.edu.br>
2021-02-06 00:08:55 -0300

    Foram modificados os layouts do @media screen e incrementados algumas classes a mais que ajudariam a estilização do site

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,7 +6,8 @@ import router from './router'
 import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-// Install BootstrapVue
+
+// Utilizando o bootstrap
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
